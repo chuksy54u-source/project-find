89d8c6c94e5ddd5149c66b0e1b58909da8616f23
Chukwuma <chuksy54u@gmal.com>
2026-09-10 08:55:24 +0100

    Updated code

---
+++ app/sitemap.js
@@ -0,0 +1,11 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://project-find.online',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    // Add more URL objects here if you have extra routes
+  ];
+}
